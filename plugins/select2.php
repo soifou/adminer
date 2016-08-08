4741fc9dfb44a1a6857d11804f321deb1e2a8f80
Soifou <francois@aparticula.com>
2016-08-08 20:19:10 +0200

    update select2

--- plugins/select2.php
+++ plugins/select2.php
@@ -1,24 +1,12 @@
 <?php
 
-/**
- * Faster tables filter plugin
- * ===========================
- * Useful when there's way too many tables than it shoud be and Adminer Tables Filter is slow
- *
- * @author Martin Macko, https://github.com/linkedlist
- * @license http://http://opensource.org/licenses/MIT, The MIT License (MIT)
- */
 class AdminerSelect2
 {
-
     function head() {
-
     ?>
-
 <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
 <script>
 jQuery(document).ready(function($) {
     initSelect2();
@@ -28,7 +16,6 @@ jQuery(document).ready(function($) {
 });
 function initSelect2() { jQuery('select').select2(); }
 </script>
-
 <?php
     }
 }
