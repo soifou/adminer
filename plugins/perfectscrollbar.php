650bfc1d0a0d2627f02ad376b0f0c208b9efe9c5
Soifou <fleur.fr@gmail.com>
2019-07-18 11:45:02 +0200

    perfectscrollbar: get rid of jquery

--- plugins/perfectscrollbar.php
+++ plugins/perfectscrollbar.php
@@ -5,12 +5,11 @@ class AdminerPerfectScrollbar
     function head() {
     ?>
 <link<?php echo nonce() ?> href="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/css/perfect-scrollbar.min.css" rel="stylesheet" />
-<script<?php echo nonce() ?> src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.slim.min.js"></script>
 <script<?php echo nonce() ?> src="https://cdnjs.cloudflare.com/ajax/libs/jquery.perfect-scrollbar/1.4.0/perfect-scrollbar.min.js"></script>
 <script<?php echo nonce() ?>>
-jQuery(document).ready(function($) {
+document.addEventListener('DOMContentLoaded', function(){
     const ps = new PerfectScrollbar('#tables');
-});
+}, false);
 </script>
 <?php
     }
